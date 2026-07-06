956d0b65970707f4c7c0b95dfdd895d37fe84699
Saravanan v <v.saravanan.c@gmail.com>
2026-07-06 22:22:05 +0530

    feat : implemented new Ui for welcome screen and removed dead code from the project

---
+++ lib/feature/welcome/presentation/widgets/welcome_bottom_widget.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/core/util/app_color.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class WelcomeBottomWidget extends StatelessWidget {
+  final List<Widget> widgets;
+  final Function(int index) onChange;
+  final PageController pageController;
+  const WelcomeBottomWidget({
+    super.key,
+    required this.widgets,
+    required this.onChange,
+    required this.pageController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: const _InvertedTopClipper(),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.4,
+        width: double.infinity,
+        decoration: const BoxDecoration(color: AppColor.scaffoldBackground),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            PageView.builder(
+              controller: pageController,
+              itemCount: widgets.length,
+              pageSnapping: true,
+              onPageChanged: onChange,
+              itemBuilder: (context, index) {
+                return widgets[index];
+              },
+            ),
+            Positioned(
+              bottom: 30,
+              child: SafeArea(
+                child: SmoothPageIndicator(
+                  controller: pageController,
+                  count: widgets.length,
+                  effect: const ExpandingDotsEffect(
+                    dotHeight: 8,
+                    dotWidth: 8,
+                    activeDotColor: AppColor.primary,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _InvertedTopClipper extends CustomClipper<Path> {
+  const _InvertedTopClipper();
+
+  final double depth = 25;
+
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.moveTo(0, 0);
+    path.cubicTo(
+      size.width * 0,
+      0,
+      size.width * 0.2,
+      depth,
+      size.width / 2,
+      depth,
+    );
+
+    path.cubicTo(size.width * 0.8, depth, size.width * 1, 0, size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant _InvertedTopClipper oldClipper) {
+    return false;
+  }
+}
---
+++ lib/feature/welcome/presentation/widgets/welcome_mobile_holder_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/core/images/app_images.dart';
+
+class WelcomeMobileHolderWidget extends StatelessWidget {
+  final int count;
+  final PageController pageController;
+  final Function(int) onChange;
+  const WelcomeMobileHolderWidget({
+    super.key,
+    required this.count,
+    required this.pageController,
+    required this.onChange,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentGeometry.topCenter,
+      children: [
+        ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 320),
+          child: Image.asset(AppImages.mock),
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: 286),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 13.0),
+            child: ClipRRect(
+              borderRadius: const BorderRadiusGeometry.vertical(
+                top: Radius.elliptical(46, 38),
+              ),
+              child: PageView.builder(
+                controller: pageController,
+                itemCount: count,
+                pageSnapping: true,
+                onPageChanged: onChange,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.orange[(index + 1) * 100],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 17,
+          child: Container(
+            width: 74,
+            height: 23,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(70),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
